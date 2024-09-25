ef2190d94d807ea97e37294a9a035c9008dcd4ea
red-edit <kate.aedon@gmail.com>
2024-09-25 12:37:54 +0200

    fixed report.js

--- report.js
+++ report.js
@@ -1,6 +1,7 @@
+
 const { execSync } = require('child_process');
 
-/
+
 const reporterId = process.env.CC_TEST_REPORTER_ID;
 if (!reporterId) {
     console.error('CC_TEST_REPORTER_ID is not defined.');
@@ -12,6 +13,6 @@ execSync(`npx codeclimate-test-reporter after-build --exit-code $?`, {
     stdio: 'inherit',
     env: {
         ...process.env,
-        CODECLIMATE_REPO_TOKEN: reporterId, // Передача правильного токена
+        CODECLIMATE_REPO_TOKEN: reporterId, 
     },
 });
