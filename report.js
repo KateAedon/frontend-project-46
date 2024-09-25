71464fc4b57378502c739237d3c1e55967fc9a1d
red-edit <kate.aedon@gmail.com>
2024-09-25 12:34:22 +0200

    added report file with correct id name

---
+++ report.js
@@ -0,0 +1,17 @@
+const { execSync } = require('child_process');
+
+/
+const reporterId = process.env.CC_TEST_REPORTER_ID;
+if (!reporterId) {
+    console.error('CC_TEST_REPORTER_ID is not defined.');
+    process.exit(1);
+}
+
+
+execSync(`npx codeclimate-test-reporter after-build --exit-code $?`, {
+    stdio: 'inherit',
+    env: {
+        ...process.env,
+        CODECLIMATE_REPO_TOKEN: reporterId, // Передача правильного токена
+    },
+});
